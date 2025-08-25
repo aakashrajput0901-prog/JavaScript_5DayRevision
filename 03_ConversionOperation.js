a73c51476d9441fd29f44483592fe34dc70c2ef0
aakashr959 <aakashr1566@gmail.com>
2025-08-25 15:56:33 +0530

    conversion

---
+++ 03_ConversionOperation.js
@@ -0,0 +1,14 @@
+let score = true;
+console.log(typeof score);
+console.log(typeof(score));
+
+let score2 = "10";
+console.log(typeof score2)
+console.log(typeof(score2))
+
+let valueInNumber = Number(score2);
+console.log(valueInNumber);
+
+
+
+
